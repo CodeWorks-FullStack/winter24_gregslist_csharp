7c5f3c93481519a916268e2685e37c0c5803cef5
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-26 09:39:36 -0600

    env setup, don't forget appsettings file on backend

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,9 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+// TODO don't forget to update your baseURL for dotnet
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+
+// TODO change to your own Auth credentials, create appsettings.Development.json in root folder of server
+export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
+export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
+export const audience = 'https://jeremyisaraddude.com'
